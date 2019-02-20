8baf57c9d9cc14074dacd3c97d308204b1590a08
Floris Lans <99028708@mydavinci.nl>
2019-02-20 10:52:41 +0100

    next and back works
    still crashes on reset

--- voting.js
+++ voting.js
@@ -11,16 +11,46 @@ var final_score = [];
 //onclick generates question innerHTML
 function Start_survey(){
     showquestion();
+    //TODO make start button dissapear on onclick
 }
 function showquestion(){
     //short is title, long is statement
-    short.innerHTML = subjects[0].title;
-    long.innerHTML =  subjects[0].statement;
+    short.innerHTML = subjects[question_number].title;
+    long.innerHTML =  subjects[question_number].statement;
 }
 
-/*counting function comes here
+//AddAnswer saves answer in array
+function AddAnswer(answer, counter){
+    answers[question_number] = answer;
+    NextQuestion(counter)
+}
 
+function NextQuestion(counter){
+    if (subjects[question_number]) {
+     if (counter == 'next') {
+         question_number++
+         showquestion()
+     } else if(counter == 'previous')  {
+         question_number--
+         showquestion()
+     }   
+    }
+}
+
+
+/*
+TODO back function? not sure if it need to go in for loop
 */
 
+/* 
+example of data.js object
+var subjects = [{
+    "title": "Bindend referendum",
+    "statement": "Er moet een bindend referendum komen, waarmee burgers door het parlement aangenomen wetten kunnen tegenhouden.",
+    "parties": [{
+            "name": "PVV",
+            "position": "pro",
+            "explanation": "Geen toelichting gegeven"
+        },
 
-
+*/ 
